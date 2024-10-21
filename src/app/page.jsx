3d50d3846a38e6c7b61ae13d5224d966e811051f
Nols05 <noelliron@gmail.com>
2024-10-21 15:11:31 +0200

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+export default async function THISISJUSTAPREVIEW() {
+    return (
+        <p>
+            This is just a showcase of TheContestHub to include in my portfolio.
+            The real repository is private and not available for public viewing.
+
+            You can visit the real website at:
+            https://thecontesthub.com
+        </p>
+    );
+}
